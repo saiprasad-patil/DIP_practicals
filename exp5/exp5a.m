129f2deb783c047072a18c933a14d6af02b318e9
DJSCE Student <DJSCE.Student@SVKMGRP.COM>
2024-03-16 12:24:58 +0530

    Exp6, exp5 mods,  readme

--- exp5/exp5a.m
+++ exp5/exp5a.m
@@ -1,49 +1,38 @@
 % Saiprasad Patil 60001200090
 clc;
 clear all;
-a = imread("wagon.jpg");
+a = imread("blobs.png");
 figure(1)
-subplot(3,2,1);
+% subplot(3,2,1);
 imshow(a);
 title("Original");
 [x,y,z] = size(a);
 robertsX = [-1,0,0,1];
 robertsY = [0,-1,1,0];
+
 for i = 1:x-1
     for j = 1:y-1
         Rrx = a(i,j,1)*robertsX(1) + a(i,j+1,1)*robertsX(2) + a(i+1,j,1)*robertsX(3) + a(i+1,j+1,1)*robertsX(4);
         Rry = a(i,j,1)*robertsY(1) + a(i,j+1,1)*robertsY(2) + a(i+1,j,1)*robertsY(3) + a(i+1,j+1,1)*robertsY(4);
         Rr = (Rrx + Rry) / 2;
+        X_img(i,j) = Rrx;
+        Y_img(i,j) = Rry;
         RobertsR(i,j) = Rr;
     end
 end
-subplot(3,2,2);
-imshow(RobertsR);
-title("R-plane after Roberts filter");
-for i = 1:x-1
-    for j = 1:y-1
-        Rgx = a(i,j,2)*robertsX(1) + a(i,j+1,2)*robertsX(2) + a(i+1,j,2)*robertsX(3) + a(i+1,j+1,2)*robertsX(4);
-        Rgy = a(i,j,2)*robertsY(1) + a(i,j+1,2)*robertsY(2) + a(i+1,j,2)*robertsY(3) + a(i+1,j+1,2)*robertsY(4);
-        Rg = (Rgx + Rgy) / 2;
-        RobertsG(i,j) = Rg;
-    end
-end
-subplot(3,2,3);
-imshow(RobertsG);
-title("G-plane after Roberts filter");
-for i = 1:x-1
-    for j = 1:y-1
-        Rbx = a(i,j,3)*robertsX(1) + a(i,j+1,3)*robertsX(2) + a(i+1,j,3)*robertsX(3) + a(i+1,j+1,3)*robertsX(4);
-        Rby = a(i,j,3)*robertsY(1) + a(i,j+1,3)*robertsY(2) + a(i+1,j,3)*robertsY(3) + a(i+1,j+1,3)*robertsY(4);
-        Rb = (Rbx + Rby) / 2;
-        RobertsB(i,j) = Rb;
-    end
-end
-subplot(3,2,4);
-imshow(RobertsB);
-title("B-plane after Roberts filter");
-Roberts = cat(3,RobertsR,RobertsG,RobertsB);
-subplot(3,2,5);
+figure(2)
+imshow(X_img)
+title("X-plane");
+figure(3)
+imshow(Y_img)
+title("Y-plane");
+% subplot(3,2,2);
+
+% imshow(RobertsR);
+% title("R-plane after Roberts filter");
+Roberts = cat(3,X_img,Y_img);
+figure(4)
+% subplot(3,2,5);
 imshow(Roberts);
 title("Image after Roberts filter");
 
---
+++ exp6/exp6.m
@@ -0,0 +1,41 @@
+% Saiprasad Patil 60001200090
+clc;
+close all;
+a=imread('images\exp6_test.png');
+[r, c]=size(a);
+g=zeros(r,c);
+figure(1); imshow(a);
+[Y,X]=ginput(1);
+temp=a(round (X), round (Y));
+g(round(X), round(Y))=1;
+for x=2:1:r-1
+    for y=2:1:c-1
+        for p=2:1:r-1
+            for q=2:1:c-1
+                if(g(p,q)==1)
+                    if a(p,q)==a(p,q+1)
+                        g(p,q+1)=1;
+                    end
+                    if a(p,q)==a(p,q-1)
+                        g(p,q-1)=1;
+                    end
+                    if a(p,q)==a(p-1,q) 
+                        g(p-1,q)=1;
+                    end
+                    if a(p,q)==a(p+1,q)
+                        g(p+1,q)=1;
+                    end
+                end 
+            end
+        end
+    end
+end
+ 
+figure(2);
+imshow(g);
+subplot(1,2,1) 
+imshow(a);
+title('Original Image');
+subplot(1,2,2) 
+imshow(g);
+title('Output');
