86ee5979e3e7063c6207d0685338f1466c0e5cfe
DJSCE Student <DJSCE.Student@SVKMGRP.COM>
2024-03-30 12:34:36 +0530

    exp7 opt, exp8 code

--- exp8/exp8.m
+++ exp8/exp8.m
@@ -1,5 +1,4 @@
-clc;
-clear all;
+% Given matrix 'a'
 a = [
     0 0 0 0 0 0 0;
     1 1 0 0 1 1 0;
@@ -10,51 +9,62 @@ a = [
     1 1 0 0 1 1 0;
     0 0 0 0 0 0 0
 ];
+
+% Given structuring element 'st'
 st = [
-     1 1;
-     0 0
+    1 1;
+    0 0
 ];
-[x,y] = size(a);
-dilation = zeros(x,y);
-erosion = zeros(x,y);
-opening = zeroes(x,y);
-%Dilation
-for i = 1:x-1
-    for j = 1:y-1
-        if (a(i,j) == st(1,1) || a(i,j+1) == st(1,2))
-            dilation(i,j) = 1;
-        end
-    end
-end
-for m = 1:x-1
-    for n = 1:y-1
-        if (a(m,n) == st(1,1) && a(m,n+1) == st(1,2))
-            erosion(m,n) = 1;
-        end
-    end
-end
 
-for i = 1:x-1
-    for j = 1:y-1
-        if (a(i,j) == st(1,1) || a(i,j+1) == st(1,2))
-            opening(i,j) = 1;
-        end
-    end
-end
-for m = 1:x-1
-    for n = 1:y-1
-        if (a(m,n) == st(1,1) && a(m,n+1) == st(1,2))
-            opening(m,n) = 1;
+% Define the size of the structuring element
+[st_rows, st_cols] = size(st);
+
+% Initialize the result matrices
+eroded_image = zeros(size(a));
+dilated_image = zeros(size(a));
+opened_image = zeros(size(a));
+closed_image = zeros(size(a));
+
+% Perform erosion operation
+for i = 1:size(a, 1) - st_rows + 1
+    for j = 1:size(a, 2) - st_cols + 1
+        if all(all(a(i:i+st_rows-1, j:j+st_cols-1) == st))
+            eroded_image(i+1, j+1) = 1;
         end
     end
 end
 
+% Perform dilation operation
+for i = 1:size(a, 1) - st_rows + 1
+    for j = 1:size(a, 2) - st_cols + 1
+        if any(any(a(i:i+st_rows-1, j:j+st_cols-1) == st))
+            dilated_image(i+1, j+1) = 1;
+        end
+    end
+end
 
+% Perform opening operation (erosion followed by dilation)
+opened_image = dilated_image;
+for i = 1:size(a, 1) - st_rows + 1
+    for j = 1:size(a, 2) - st_cols + 1
+        if all(all(eroded_image(i:i+st_rows-1, j:j+st_cols-1) == st))
+            opened_image(i+1, j+1) = 1;
+        end
+    end
+end
 
-disp("Saiprasad Patil 60001200090");
-disp("Original:");
-disp(a);
-disp("Dilation:");
-disp(dilation);
-disp("Erosion:");
-disp(erosion);
+% Perform closing operation (dilation followed by erosion)
+closed_image = eroded_image;
+for i = 1:size(a, 1) - st_rows + 1
+    for j = 1:size(a, 2) - st_cols + 1
+        if any(any(dilated_image(i:i+st_rows-1, j:j+st_cols-1) == st))
+            closed_image(i+1, j+1) = 1;
+        end
+    end
+end
+
+% Display results
+subplot(2, 2, 1); imshow(eroded_image); title('Eroded Image');
+subplot(2, 2, 2); imshow(dilated_image); title('Dilated Image');
+subplot(2, 2, 3); imshow(opened_image); title('Opened Image');
+subplot(2, 2, 4); imshow(closed_image); title('Closed Image');
