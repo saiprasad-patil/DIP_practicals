39793911c54dbf6713b61afefc3f527ccabb5af4
saiprasad-patil <saiprasad30042003@gmail.com>
2024-04-05 21:25:54 +0530

    exp8 final

--- exp7/exp7.m
+++ exp7/exp7.m
@@ -1,36 +1,90 @@
-% Exp7: Discrete Cosine Transform
-
+%Saiprasad Patil 60001200090
 clc;
 clear all;
-% Original  Matrix
 a = [
-    0 1 2 1;
-    1 2 3 2;
-    2 3 4 3;
-    1 2 3 2
+    0 0 0 0 0 0 0;
+    1 1 0 0 1 1 0;
+    1 1 0 0 1 1 0;
+    1 1 1 1 1 1 0;
+    1 1 1 1 1 1 0;
+    1 1 0 0 1 1 0;
+    1 1 0 0 1 1 0;
+    0 0 0 0 0 0 0
 ];
-
-t = zeros(4, 4);
-N = 4;
-
-% Calculating DCT Transformation Matrix
-for u = 1:N
-    for v = 1:N
-        if u == 1
-            t(u,v) = 1/sqrt(N);
-        else
-            t(u,v) = (sqrt(2/N)) * cos((((2*(v-1))+1)*pi*(u-1))/(2*N));
+st = [
+     1 1;
+     0 0
+];
+[x,y] = size(a);
+% Dilation
+dilation = zeros(x,y);
+for i = 1:x-1
+    for j = 1:y-1
+        if (a(i,j) == st(1,1) || a(i,j+1) == st(1,2))
+            dilation(i,j) = 1;
+        end
+    end
+end
+% Erosion
+erosion = zeros(x,y);
+for m = 1:x-1
+    for n = 1:y-1
+        if (a(m,n) == st(1,1) && a(m,n+1) == st(1,2))
+            erosion(m,n) = 1;
+        end
+    end
+end
+% Opening
+e_a = zeros(x,y);
+opening = zeros(x,y);
+for m = 1:x-1
+    for n = 1:y
+        if (a(m,n) == st(1,1) && a(m+1,n) == st(2,1))
+            e_a(m,n) = 1;
+        end
+    end
+end
+for i = 1:x-1
+    for j = 1:y
+        if (e_a(i,j) == st(1,1) || e_a(i+1,j) == st(2,1))
+            opening(i,j) = 1;
+        end
+    end
+end
+% Closing
+d_a = zeros(x,y);
+closing = zeros(x,y);
+for m = 1:x-1
+    for n = 1:y
+        if (a(m,n) == st(1,1) || a(m+1,n) == st(2,1))
+            d_a(m,n) = 1;
+        end
+    end
+end
+for i = 1:x-1
+    for j = 1:y
+        if (d_a(i,j) == st(1,1) && d_a(i+1,j) == st(2,1))
+            closing(i,j) = 1;
         end
     end
 end
 
-% Calculating DCT Matrix
-dct = t*a*transpose(t);
-
-disp("Saiprasad Patil 60001200090")
-disp("Original matrix:");
+disp("Saiprasad Patil 60001200090");
+disp("Original:");
 disp(a);
-disp("DCT transformation matrix(T):");
-disp(t);
-disp("DCT matrix:");
-disp(dct);
+disp("Structuring element:");
+disp(st);
+disp("Dilation:");
+disp(dilation);
+disp("\n");
+disp(imdilate(a,st));
+##disp("Erosion:");
+##disp(erosion);
+##disp("E(A):");
+##disp(e_a);
+##disp("After Opening:");
+##disp(opening);
+##disp("D(A):");
+##disp(d_a);
+##disp("After Closing:");
+##disp(closing);
--- exp8/exp8.m
+++ exp8/exp8.m
@@ -1,70 +1,36 @@
-% Given matrix 'a'
+% Exp7: Discrete Cosine Transform
+
+clc;
+clear all;
+% Original  Matrix
 a = [
-    0 0 0 0 0 0 0;
-    1 1 0 0 1 1 0;
-    1 1 0 0 1 1 0;
-    1 1 1 1 1 1 0;
-    1 1 1 1 1 1 0;
-    1 1 0 0 1 1 0;
-    1 1 0 0 1 1 0;
-    0 0 0 0 0 0 0
+    0 1 2 1;
+    1 2 3 2;
+    2 3 4 3;
+    1 2 3 2
 ];
 
-% Given structuring element 'st'
-st = [
-    1 1;
-    0 0
-];
+t = zeros(4, 4);
+N = 4;
 
-% Define the size of the structuring element
-[st_rows, st_cols] = size(st);
-
-% Initialize the result matrices
-eroded_image = zeros(size(a));
-dilated_image = zeros(size(a));
-opened_image = zeros(size(a));
-closed_image = zeros(size(a));
-
-% Perform erosion operation
-for i = 1:size(a, 1) - st_rows + 1
-    for j = 1:size(a, 2) - st_cols + 1
-        if all(all(a(i:i+st_rows-1, j:j+st_cols-1) == st))
-            eroded_image(i+1, j+1) = 1;
+% Calculating DCT Transformation Matrix
+for u = 1:N
+    for v = 1:N
+        if u == 1
+            t(u,v) = 1/sqrt(N);
+        else
+            t(u,v) = (sqrt(2/N)) * cos((((2*(v-1))+1)*pi*(u-1))/(2*N));
         end
     end
 end
 
-% Perform dilation operation
-for i = 1:size(a, 1) - st_rows + 1
-    for j = 1:size(a, 2) - st_cols + 1
-        if any(any(a(i:i+st_rows-1, j:j+st_cols-1) == st))
-            dilated_image(i+1, j+1) = 1;
-        end
-    end
-end
+% Calculating DCT Matrix
+dct = t*a*transpose(t);
 
-% Perform opening operation (erosion followed by dilation)
-opened_image = dilated_image;
-for i = 1:size(a, 1) - st_rows + 1
-    for j = 1:size(a, 2) - st_cols + 1
-        if all(all(eroded_image(i:i+st_rows-1, j:j+st_cols-1) == st))
-            opened_image(i+1, j+1) = 1;
-        end
-    end
-end
-
-% Perform closing operation (dilation followed by erosion)
-closed_image = eroded_image;
-for i = 1:size(a, 1) - st_rows + 1
-    for j = 1:size(a, 2) - st_cols + 1
-        if any(any(dilated_image(i:i+st_rows-1, j:j+st_cols-1) == st))
-            closed_image(i+1, j+1) = 1;
-        end
-    end
-end
-
-% Display results
-subplot(2, 2, 1); imshow(eroded_image); title('Eroded Image');
-subplot(2, 2, 2); imshow(dilated_image); title('Dilated Image');
-subplot(2, 2, 3); imshow(opened_image); title('Opened Image');
-subplot(2, 2, 4); imshow(closed_image); title('Closed Image');
+disp("Saiprasad Patil 60001200090")
+disp("Original matrix:");
+disp(a);
+disp("DCT transformation matrix(T):");
+disp(t);
+disp("DCT matrix:");
+disp(dct);
